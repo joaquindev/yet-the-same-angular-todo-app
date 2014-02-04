71f0b2ab1239cc1a63054d5f46095cd47ed6ebfb
JoaquinDev <joaquin@isla.io>
2014-02-04 10:58:30 +0100

    Add first backend route: /api/todos
    Add first backend route: /api/todos. Now there is an error when clicking
    the Add Todo button because when the code gets to Todo object, it is not
    defined (inside our routes.js). So we 'require' it but now we have to
    defined our Todo model. NEXT: Define our Todo Model
    
    Contact: joaquin@isla.io

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,29 @@
 'use strict';
-//var Todo = require('./models/todo');
-module.exports = function(app){}; 
+var Todo = require('./models/todo');
+
+module.exports = function(app){
+    //SERVER Routes
+    app.get('/api/todos', function(req,res){
+        Todo.find(function(err, todos){
+            if (err) res.send(err);
+            res.json(todos);
+        });
+    });
+
+
+    app.post('/api/todos', function(req,res){
+        Todo.create({
+            text: req.body.text,
+            done: false
+        }, function(err, todo){
+            if (err) res.send(err);
+            Todo.find(function(err,todos){
+                if (err) res.send(err);
+                res.json(todos);
+            });
+        });
+    });
+
+    //FRONTEND Routes
+    //TODO
+};
